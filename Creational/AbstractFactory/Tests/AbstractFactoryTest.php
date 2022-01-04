0c034277b9d0490116699d1bbaa34a440f5e632b
diwakar.kumar@mpokket.com <diwakar.kumar@mpokket.com>
2022-01-04 20:14:10 +0530

    Abstract factory test file created

---
+++ Creational/AbstractFactory/Tests/AbstractFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace DesignPatternsPHP\Creational\AbstractFactory\Tests;
+
+use DesignPatternsPHP\Creational\AbstractFactory\CsvWriter;
+use DesignPatternsPHP\Creational\AbstractFactory\JsonWriter;
+use DesignPatternsPHP\Creational\AbstractFactory\UnixWriterFactory;
+use DesignPatternsPHP\Creational\AbstractFactory\WinWriterFactory;
+use DesignPatternsPHP\Creational\AbstractFactory\WriterFactory;
+use PHPUnit\Framework\TestCase;
+
+class AbstractFactoryTest extends TestCase{
+
+    public function provideFactory()
+    {
+        return [
+            [new UnixWriterFactory()],
+            [new WinWriterFactory()]
+        ];
+
+    }
+
+    /**
+     * @dataProvider provideFactory
+     */
+
+    public function testCanCreateCsvWriterOnUnix(WriterFactory $writerFactory)
+    {
+        $this->assertInstanceOf(JsonWriter::class, $writerFactory->createJsonWriter());
+        $this->assertInstanceOf(CsvWriter::class, $writerFactory->createCsvWriter());
+
+    }
+}
